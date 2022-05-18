63f68ec39d1f8a0c6f42a87315fe4b0d00c92731
milanese-samuele <s.milanese@student.rug.nl>
2022-05-18 13:05:17 +0200

    working script

--- week 1/main.py
+++ week 1/main.py
@@ -1,8 +1,7 @@
-# This is a sample Python script.
+#!/usr/bin/env python
+# coding: utf-8
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from keras_uncertainty.losses import regression_gaussian_nll_loss, regression_gaussian_beta_nll_loss
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli, norm, poisson
@@ -10,14 +9,18 @@ from scipy import stats
 from scipy.stats import kurtosis, skew
 import seaborn as sns
 import math
+from keras.layers import Dense, Input, Flatten
 
 import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 import keras_uncertainty.backend as K
+
+tf.compat.v1.disable_eager_execution()
 
 # Losses commonly used in uncertainty quantification and probabilistic forecasting
 
@@ -33,38 +36,62 @@ def regression_gaussian_nll_loss(variance_tensor, epsilon=1e-8, variance_logits=
 
         return 0.5 * K.mean(K.log(variance_tensor + epsilon) + K.square(y_true - y_pred) / (variance_tensor + epsilon))
 
-    return nl
+    return nll
 
-def train_model(inputs, outputs, test_set):
-    train_dataset = inputs
-    test_dataset = test_set
-    input = keras.layers.Input(shape=inputs.shape)
-    x = keras.layers.Dense(32, activation="relu")(input)
-    # output = keras.layers.Dense(1, activation="relu")(x)
+def train_standard_model(x_train, y_train, domain):
+    inp = Input(shape=(1,))
+    x = Dense(32, activation="relu")(inp)
+    x = Dense(32, activation="relu")(x)
+    mean = Dense(1, activation="linear")(x)
+    var = Dense(1, activation="softplus")(x)
 
-    miu = keras.layers.Dense(1, activation="relu")(x)
-    var = keras.layers.Dense(1, activation="softplus")(x)
+    train_model = Model(inp, mean)
+    pred_model = Model(inp, [mean, var])
 
-    training_model = Model(input, miu)
-    testing_model = Model(input, [miu, var])
+    train_model.compile(loss=regression_gaussian_nll_loss(var), optimizer="adam")
+    train_model.fit(x_train, y_train, verbose=2, epochs=100)
 
-    training_model.summary()
+    mean_pred, var_pred = pred_model.predict(domain)
+    std_pred = np.sqrt(var_pred)
 
-    training_model.compile(loss=out_loss, optimizer="adam")
+    return mean_pred, std_pred
 
-    return training_model, testing_model
 
-x = np.linspace(-5, 5, num=100)
-print("Input array : \n", x)
+num_samples = 10000
 
-x = np.sin(x)
+sample = np.linspace(-5, 5, num=num_samples)
+print("Input array : \n", sample)
+
+x = np.sin(sample) + np.random.normal(loc = 0.0, scale = 1.0, size = num_samples)
 print("\nSine values : \n", x)
 
-y = np.random.choice(x, 100, replace=False)
-y = x + np.random.normal(loc = 0.0, scale = 2.0)
+y = np.sin(sample)
+y = y.reshape((-1, 1))
 # X = np.random.normal(loc = 0.0, scale = 2.0, size = 1000)
+print("shape of y", y.shape)
+
+data_train,data_test,labels_train,labels_test = train_test_split(x,y, test_size = 0.20)
 
 print(y)
 print(f'μ={y.mean()}')
 print(f'σ={y.std()}')
 
+predicted_mean, predicted_var=train_standard_model(data_train, labels_train, y)
+
+print("pred mean", predicted_mean)
+print("pred var", predicted_var)
+
+y_pred_mean = predicted_mean.reshape((-1,))
+y_pred_std = predicted_var.reshape((-1,))
+y_pred_up_1 = y_pred_mean + y_pred_std
+y_pred_down_1 = y_pred_mean - y_pred_std
+
+
+plt.plot(y, label= "sinus")
+plt.plot(y_pred_mean, label = "predicted mean")
+plt.fill_between (range (num_samples), y_pred_mean-y_pred_std, y_pred_mean+y_pred_std, alpha=0.2, label="std")
+plt.scatter (range (len (x)), x, label="noisy data")
+# plt.plot(y_pred_up_1, label = "one std up")
+# plt.plot(y_pred_down_1, label = "one std below")
+plt.legend()
+plt.show ()
